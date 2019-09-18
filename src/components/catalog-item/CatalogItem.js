962b95141fbd1f66cbe196812bea4a7031edbf16
mliqian <qianqianio@qq.com>
2019-09-18 11:17:28 +0800

    Add styles to catalog

--- src/components/catalog-item/CatalogItem.js
+++ src/components/catalog-item/CatalogItem.js
@@ -1,7 +1,19 @@
 import React from "react";
+import "./catalog-item.scss";
 
-function CatalogItem() {
-  return <h1>CatalogItem</h1>;
+function CatalogItem({ title, background, size }) {
+  const bgStyle = {
+    backgroundImage: `url(${background})`
+  };
+  return (
+    <div className={`catalog-item ${size}`}>
+      <div className="catalog-item__bg" style={bgStyle}></div>
+      <div className="catalog-item__content">
+        <h1>{title}</h1>
+        <span>desc</span>
+      </div>
+    </div>
+  );
 }
 
 export default CatalogItem;
